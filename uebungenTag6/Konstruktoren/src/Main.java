00c7e562ee5127f2c4c4d63414802a632d5645bd
Alexandru Schneider <schneider.alex@icloud.com>
2026-04-13 11:52:58 +0200

    Konstruktoren

---
+++ uebungenTag6/Konstruktoren/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+       User user =  new User("ALex", 10);
+
+        user.ausgabe();
+
+        User user2 = new User();
+        user2.ausgabe();
+
+    }
+}
---
+++ uebungenTag6/Konstruktoren/src/User.java
@@ -0,0 +1,23 @@
+public class User {
+
+String name;
+int beiträge;
+
+    public User (String name, int beiträge) {
+        this.name = name;
+        this.beiträge = beiträge;
+
+    }
+
+    public User() {
+        this.name = "Unbekant";
+        this.beiträge = 0;
+
+
+    }
+    public void ausgabe() {
+        IO.println("Benutzer: " + name + ", Beiträge: " + beiträge);
+    }
+
+
+}
