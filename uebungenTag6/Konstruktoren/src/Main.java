33167adf8bb2b8e17ef2791d7ae34872e8383d25
Alexandru Schneider <schneider.alex@icloud.com>
2026-04-13 11:53:57 +0200

    Konstruktoren

--- uebungenTag6/Konstruktoren/src/Main.java
+++ uebungenTag6/Konstruktoren/src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-       User user =  new User("ALex", 10);
+       User user1 =  new User("ALex", 10);
 
-        user.ausgabe();
+        user1.ausgabe();
 
         User user2 = new User();
         user2.ausgabe();
